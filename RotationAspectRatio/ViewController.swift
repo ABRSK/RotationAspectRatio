59c26459fcbef6de3563647523de437deff8ce04
AreyBRSK <areybrsk@gmail.com>
2022-03-22 19:09:57 +0300

    Initial Commit

---
+++ RotationAspectRatio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RotationAspectRatio
+//
+//  Created by Андрей Барсук on 22.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
