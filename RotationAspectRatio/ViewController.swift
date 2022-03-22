a7239e27b819ddc33814fd39f4348aa0c05c2ace
AreyBRSK <areybrsk@gmail.com>
2022-03-22 22:59:24 +0300

    Solved

--- RotationAspectRatio/ViewController.swift
+++ RotationAspectRatio/ViewController.swift
@@ -8,10 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var greenBox: UIView!
+    @IBOutlet var redBox: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        greenBox.layer.cornerRadius = 20
+        redBox.layer.cornerRadius = 20
     }
 
 
